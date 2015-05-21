783cad878d5f53cd7e225bd17117011b69ae926a
Taichi Uragami <backpaper0@gmail.com>
2015-05-22 06:14:09 +0900

    Initial commit

---
+++ src/main/java/app/hello/HelloController.java
@@ -0,0 +1,13 @@
+package app.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello, world!";
+    }
+}
