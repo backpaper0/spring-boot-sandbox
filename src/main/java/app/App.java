c41b6fd40838e2320b45eb2ce3d0416e391287d7
Taichi Uragami <backpaper0@gmail.com>
2015-05-22 21:12:02 +0900

    バッチを試す

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,12 @@
 package app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Batchを使うときに必要になる
+@EnableBatchProcessing
 public class App {
 
     public static void main(String[] args) {
