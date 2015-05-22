2aa203c0ccbf454ae9ac0eb5b5936f5bdf11261a
Taichi Uragami <backpaper0@gmail.com>
2015-05-23 04:57:32 +0900

    ラムダ式でええやん( ｀・∀・´)ﾉ

--- src/main/java/app/batch/SimpleBatch.java
+++ src/main/java/app/batch/SimpleBatch.java
@@ -4,10 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,15 +35,10 @@ public class SimpleBatch {
     @Bean
     public Job job() throws Exception {
 
-        Tasklet tasklet = new Tasklet() {
-
-            @Override
-            public RepeatStatus execute(StepContribution contribution,
-                    ChunkContext chunkContext) throws Exception {
-                Logger logger = LoggerFactory.getLogger(SimpleBatch.class);
-                logger.info("Hello, Spring Batch!!");
-                return RepeatStatus.FINISHED;
-            }
+        Tasklet tasklet = (contribution, chunkContext) -> {
+            Logger logger = LoggerFactory.getLogger(SimpleBatch.class);
+            logger.info("Hello, Spring Batch!!");
+            return RepeatStatus.FINISHED;
         };
 
         Step step = steps.get("step").tasklet(tasklet).build();
