95c01926fc1b9dc0f2c05e4d3462e9a7fc30a12e
Taichi Uragami <backpaper0@gmail.com>
2016-07-11 22:50:08 +0900

    とりあえずSpring Initializrで作ったやつをどんする

---
+++ spring-boot-doma-study/src/main/java/com/example/SpringBootDomaStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDomaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDomaStudyApplication.class, args);
+	}
+}
