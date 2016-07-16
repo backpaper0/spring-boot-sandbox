c8af66f8b2ca049cd71bb66429ff64962b2dba4b
Taichi Uragami <backpaper0@gmail.com>
2016-07-16 20:20:11 +0900

    雑にJavadoc付けた

--- spring-boot-doma-study/src/main/java/study/controller/TodoController.java
+++ spring-boot-doma-study/src/main/java/study/controller/TodoController.java
@@ -27,26 +27,58 @@ public class TodoController {
         this.service = service;
     }
 
+    /**
+     * 全件取ってくる。
+     * 
+     * @return
+     */
     @RequestMapping(method = RequestMethod.GET)
     List<Todo> findAll() {
         return service.findAll();
     }
 
+    /**
+     * idで1件だけ取ってくる。
+     * 無ければ404 Not Found。
+     * 
+     * @param id
+     * @return
+     * @see #notFound(NoSuchElementException)
+     */
     @RequestMapping(method = RequestMethod.GET, value = "{id}")
     Todo findById(@PathVariable Long id) {
         return service.findById(id).get();
     }
 
+    /**
+     * Todoを作る。
+     * 
+     * @param content
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST)
     Todo create(@RequestParam String content) {
         return service.create(content);
     }
 
+    /**
+     * 渡されたidに該当するTodoを終了する。
+     * 無ければ404 Not Found。
+     * 
+     * @param id
+     * @return
+     * @see #notFound(NoSuchElementException)
+     */
     @RequestMapping(method = RequestMethod.POST, value = "{id}:close")
     Todo close(@PathVariable Long id) {
         return service.close(id).get();
     }
 
+    /**
+     * NoSuchElementExceptionが投げられたら404 Not Foundにする。
+     * 
+     * @param e
+     */
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     void notFound(NoSuchElementException e) {
