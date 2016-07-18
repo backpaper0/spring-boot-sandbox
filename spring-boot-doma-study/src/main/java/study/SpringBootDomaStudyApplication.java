fcd3ddc22f7e9e75c04bb60a00353b005c8f22c1
Taichi Uragami <backpaper0@gmail.com>
2016-07-18 12:58:07 +0900

    SimpleModuleをコンポーネント定義する事でJacksonにSerializerを登録する
    https://twitter.com/making/status/754877881239474176

--- spring-boot-doma-study/src/main/java/study/SpringBootDomaStudyApplication.java
+++ spring-boot-doma-study/src/main/java/study/SpringBootDomaStudyApplication.java
@@ -1,19 +1,17 @@
 package study;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import study.domain.Content;
 import study.domain.Key;
@@ -26,30 +24,25 @@ public class SpringBootDomaStudyApplication {
     }
 
     @Bean
-    ObjectMapper objectMapper() {
-        return Jackson2ObjectMapperBuilder.json()
-                .serializerByType(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
-                    @Override
-                    public void serialize(LocalDateTime value, JsonGenerator jgen,
-                            SerializerProvider provider)
-                            throws IOException, JsonProcessingException {
-                        jgen.writeString(value.toString());
-                    }
-                }).serializerByType(Key.class, new JsonSerializer<Key<?>>() {
-                    @Override
-                    public void serialize(Key<?> value, JsonGenerator jgen,
-                            SerializerProvider provider)
-                            throws IOException, JsonProcessingException {
-                        jgen.writeNumber(value.getValue());
-                    }
-                }).serializerByType(Content.class, new JsonSerializer<Content>() {
-                    @Override
-                    public void serialize(Content value, JsonGenerator jgen,
-                            SerializerProvider provider)
-                            throws IOException, JsonProcessingException {
-                        jgen.writeString(value.getValue());
-                    }
-                }).build();
+    SimpleModule module() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Key.class, new JsonSerializer<Key>() {
+            @Override
+            public void serialize(Key value, JsonGenerator jgen,
+                    SerializerProvider provider)
+                    throws IOException, JsonProcessingException {
+                jgen.writeNumber(value.getValue());
+            }
+        });
+        module.addSerializer(Content.class, new JsonSerializer<Content>() {
+            @Override
+            public void serialize(Content value, JsonGenerator jgen,
+                    SerializerProvider provider)
+                    throws IOException, JsonProcessingException {
+                jgen.writeString(value.getValue());
+            }
+        });
+        return module;
     }
 
     @Bean
