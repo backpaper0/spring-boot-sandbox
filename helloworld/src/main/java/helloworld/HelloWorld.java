b782d42d1b08b870eb65ee5ef972330201eb8181
Taichi Uragami <backpaper0@gmail.com>
2016-07-19 21:48:06 +0900

    スタックトレースで動きを探る

--- helloworld/src/main/java/helloworld/HelloWorld.java
+++ helloworld/src/main/java/helloworld/HelloWorld.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+    public HelloWorld() {
+        new Throwable().printStackTrace(System.out);
+    }
+
     @RequestMapping("/hello")
     public String sayHello() {
+        new Throwable().printStackTrace(System.out);
         return "Hello world!";
     }
 }
