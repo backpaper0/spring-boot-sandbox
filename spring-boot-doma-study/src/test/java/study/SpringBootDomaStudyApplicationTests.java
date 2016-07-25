ae582b955c775c1c1d564847c86ed2f85a5b932d
Taichi Uragami <backpaper0@gmail.com>
2016-07-26 05:27:05 +0900

    WebIntegrationTestを試す

--- spring-boot-doma-study/src/test/java/study/SpringBootDomaStudyApplicationTests.java
+++ spring-boot-doma-study/src/test/java/study/SpringBootDomaStudyApplicationTests.java
@@ -1,18 +1,40 @@
 package study;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import study.entity.Todo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootDomaStudyApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest
 public class SpringBootDomaStudyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private final RestTemplate template = new TestRestTemplate();
 
+    @Test
+    public void testTodoList() throws Exception {
+        RequestEntity<?> request = new RequestEntity<>(HttpMethod.GET,
+                URI.create("http://localhost:8080/todo"));
+        ResponseEntity<List<Todo>> response = template.exchange(request,
+                new ParameterizedTypeReference<List<Todo>>() {
+                });
+        assertThat(response.getBody(), is(Collections.emptyList()));
+    }
 }
