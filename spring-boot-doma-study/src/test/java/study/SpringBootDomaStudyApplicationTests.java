ac5d6fcc640cf57148160fc3d9c6e5870afae9e0
Taichi Uragami <backpaper0@gmail.com>
2016-07-26 20:35:58 +0900

    Upgrade Spring Boot 1.3.6.RELEASE -> 1.4.0.RC1

--- spring-boot-doma-study/src/test/java/study/SpringBootDomaStudyApplicationTests.java
+++ spring-boot-doma-study/src/test/java/study/SpringBootDomaStudyApplicationTests.java
@@ -3,36 +3,35 @@ package study;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import study.entity.Todo;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootDomaStudyApplication.class)
-@WebIntegrationTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SpringBootDomaStudyApplicationTests {
 
-    private final RestTemplate template = new TestRestTemplate();
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     public void testTodoList() throws Exception {
-        RequestEntity<?> request = new RequestEntity<>(HttpMethod.GET,
-                URI.create("http://localhost:8080/todo"));
-        ResponseEntity<List<Todo>> response = template.exchange(request,
+        ResponseEntity<List<Todo>> response = template.exchange(
+                "/todo",
+                HttpMethod.GET,
+                null,
                 new ParameterizedTypeReference<List<Todo>>() {
                 });
         assertThat(response.getBody(), is(Collections.emptyList()));
