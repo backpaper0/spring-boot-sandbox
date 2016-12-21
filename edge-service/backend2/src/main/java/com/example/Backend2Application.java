26bf2e03141e7dbbb875ce78b1b4163678718270
Taichi Uragami <backpaper0@gmail.com>
2016-12-22 05:23:25 +0900

    Service Discoveryを合わせてみる

--- edge-service/backend2/src/main/java/com/example/Backend2Application.java
+++ edge-service/backend2/src/main/java/com/example/Backend2Application.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class Backend2Application {
 
--- edge-service/frontend/src/main/java/com/example/FrontendApplication.java
+++ edge-service/frontend/src/main/java/com/example/FrontendApplication.java
@@ -4,12 +4,15 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FrontendApplication {
 
     public static void main(String[] args) {
@@ -17,6 +20,7 @@ public class FrontendApplication {
     }
 
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
