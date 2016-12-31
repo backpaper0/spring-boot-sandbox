990da3674b2aeb9b76c20ce45d83bffce350feb1
Taichi Uragami <backpaper0@gmail.com>
2016-12-31 09:22:30 +0900

    Distributed Tracing

---
+++ distributed-tracing/service3/src/main/java/com/example/BackendApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "Hello World";
+    }
+}
