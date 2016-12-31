ac175dab3ad2c6dc6b78cda14154e83268512f31
Taichi Uragami <backpaper0@gmail.com>
2016-12-31 09:33:35 +0900

    OAuth 2でSSO

--- oauth2/helloworld-api/src/main/java/com/example/HelloWorldAPIApplication.java
+++ oauth2/helloworld-api/src/main/java/com/example/HelloWorldAPIApplication.java
@@ -1,25 +1,23 @@
 package com.example;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 @EnableResourceServer
 @RestController
-public class AuthorizationApplication {
+public class HelloWorldAPIApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationApplication.class, args);
+        SpringApplication.run(HelloWorldAPIApplication.class, args);
     }
 
-    @GetMapping("/userinfo")
-    Authentication userinfo(Authentication authentication) {
-        return authentication;
+    @GetMapping("/hello")
+    String hello(Principal principal) {
+        return String.format("Hello, %s!", principal.getName());
     }
 }
---
+++ oauth2/helloworld-ui/src/main/java/com/example/HelloWorldUIApplication.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class HelloWorldUIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldUIApplication.class, args);
+    }
+
+    @Bean
+    OAuth2RestTemplate restTemplate(OAuth2ProtectedResourceDetails resource,
+            OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
+    }
+}
+
+@Controller
+class HelloController {
+
+    final OAuth2RestTemplate restTemplate;
+
+    public HelloController(OAuth2RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/")
+    String hello(Model model) {
+        model.addAttribute("hello",
+                restTemplate.getForObject("http://localhost:8000/hello", String.class));
+        return "hello";
+    }
+}
