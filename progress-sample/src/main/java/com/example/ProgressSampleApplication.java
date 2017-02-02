c27e30f32a3cf1dbd5f1eb1fd5f9d26f1727ecc9
Taichi Uragami <backpaper0@gmail.com>
2017-02-03 05:51:51 +0900

    GETで処理走らせるのは気持ち悪かったからPOSTで走らせるようにした

--- progress-sample/src/main/java/com/example/ProgressSampleApplication.java
+++ progress-sample/src/main/java/com/example/ProgressSampleApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.DoubleConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +12,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @EnableAsync
@@ -28,43 +35,67 @@ class SampleController {
 
     @Autowired
     SampleService service;
+    @Autowired
+    Progress progress;
 
     @GetMapping("/")
     String index() {
         return "index";
     }
 
-    @GetMapping("/run")
-    SseEmitter run() throws Exception {
+    @PostMapping("/run")
+    @ResponseBody
+    String run() {
+        UUID key = UUID.randomUUID();
+        service.post(a -> progress.getListener(key).accept(a));
+        return key.toString();
+    }
+
+    @GetMapping("/progress/{name}")
+    SseEmitter progress(@PathVariable String name) {
+        UUID key = UUID.fromString(name);
         SseEmitter emitter = new SseEmitter();
-        service.post(a -> {
+        progress.addListener(key, a -> {
             try {
-                if (a < 1d) {
-                    emitter.send(a);
-                } else {
-                    emitter.send(1.0d);
-                    emitter.complete();
-                }
+                emitter.send(a);
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             }
+            if (a >= 1) {
+                emitter.complete();
+            }
         });
+        emitter.onCompletion(() -> progress.removeListener(key));
         return emitter;
+    }
+}
+
+@Component
+class Progress {
+    final ConcurrentMap<UUID, DoubleConsumer> listeners = new ConcurrentHashMap<>();
+    void addListener(UUID key, DoubleConsumer listener) {
+        listeners.put(key, listener);
+    }
+    void removeListener(UUID key) {
+        listeners.remove(key);
+    }
+    DoubleConsumer getListener(UUID key) {
+        return listeners.getOrDefault(key, a -> {
+        });
     }
 }
 
 @Service
 class SampleService {
-
     @Async
     public void post(DoubleConsumer consumer) {
         for (int i = 0; i < 100; i++) {
-            consumer.accept(i / 100d);
+            consumer.accept(i * 0.01);
             try {
                 TimeUnit.MILLISECONDS.sleep(50);
             } catch (InterruptedException e) {
             }
         }
-        consumer.accept(1d);
+        consumer.accept(1);
     }
 }
---
+++ progress-sample/src/main/resources/static/index.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#app',
+  data: {
+    progress: 0
+  },
+  methods: {
+    start () {
+      //let csrfHeaderName = document.getElementsByName('_csrf_header')[0].getAttribute('content')
+      //let csrfToken = document.getElementsByName('_csrf')[0].getAttribute('content')
+      //let headers = new Headers()
+      //headers.append(csrfHeaderName, csrfToken)
+      let req = new Request('run', { method: 'POST'/*, headers */ })
+      fetch(req)
+        .then(res => res.text())
+        .then(name => {
+          var es = new EventSource('progress/' + name)
+          es.onmessage = e => {
+            let value = parseFloat(e.data)
+            this.progress = value * 100
+            if (value >= 1) {
+              es.close()
+            }
+          }
+        })
+    }
+  }
+})
