1d34cb1e68dfd21becce059c30b54cf1d46fa04a
Taichi Uragami <backpaper0@gmail.com>
2017-07-05 20:10:49 +0900

    enum
    enumはデフォルトでvalueOfメソッドが定義されているので
    ObjectToObjectConverter#getValidatedMemberがダメなんじゃね？
    と思いアレした。

--- spring-sandbox/src/main/java/study/requestparam/RequestParamSample.java
+++ spring-sandbox/src/main/java/study/requestparam/RequestParamSample.java
@@ -1,7 +1,20 @@
 package study.requestparam;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.ConditionalGenericConverter;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +27,19 @@ public class RequestParamSample {
         SpringApplication.run(RequestParamSample.class, args);
     }
 
-    //curl "localhost:8080?a=aaa&b=bbb&c=ccc&d=ddd"
+    //curl "localhost:8080?a=aaa&b=bbb&c=ccc&d=ddd&e=eee&f=fff"
+
+    // see: ObjectToObjectConverter#getValidatedMember
 
     @GetMapping("/")
     String get(
             @RequestParam final ValueOf a,
             @RequestParam final Of b,
             @RequestParam final From c,
-            @RequestParam final Constructor d) {
-        return String.format("%s, %s, %s, %s", a, b, c, d);
+            @RequestParam final Constructor d,
+            @RequestParam final EnumValueOf e,
+            @RequestParam final EnumOf f) {
+        return String.format("%s, %s, %s, %s, %s, %s", a, b, c, d, e, f);
     }
 }
 
@@ -67,5 +84,75 @@ class Constructor {
     @Override
     public String toString() {
         return s;
+    }
+}
+
+enum EnumValueOf {
+    eee, xxx, yyy;
+    public static EnumValueOf of(final String s) {
+        return xxx;
+    }
+    public static EnumValueOf from(final String s) {
+        return yyy;
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface EnumOfConverted {
+}
+
+@EnumOfConverted
+enum EnumOf {
+    SINGLETON("fff");
+    private final String s;
+    EnumOf(final String s) {
+        this.s = s;
+    }
+    @Override
+    public String toString() {
+        return s;
+    }
+    public static EnumOf of(final String s) {
+        return Arrays.stream(values())
+                .filter(a -> Objects.equals(s, a.s))
+                .findAny()
+                .orElse(null);
+    }
+}
+
+@Component
+class EnumOfConverter implements ConditionalGenericConverter, InitializingBean {
+
+    private final GenericConversionService service;
+
+    public EnumOfConverter(final GenericConversionService service) {
+        this.service = Objects.requireNonNull(service);
+    }
+
+    @Override
+    public Set<ConvertiblePair> getConvertibleTypes() {
+        return Collections.singleton(new ConvertiblePair(String.class, Enum.class));
+    }
+
+    @Override
+    public Object convert(final Object source, final TypeDescriptor sourceType,
+            final TypeDescriptor targetType) {
+        final Method method = ReflectionUtils.findMethod(targetType.getType(), "of", String.class);
+        ReflectionUtils.makeAccessible(method);
+        try {
+            return method.invoke(null, source);
+        } catch (final ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean matches(final TypeDescriptor sourceType, final TypeDescriptor targetType) {
+        return targetType.getType().getAnnotation(EnumOfConverted.class) != null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        service.addConverter(this);
     }
 }
