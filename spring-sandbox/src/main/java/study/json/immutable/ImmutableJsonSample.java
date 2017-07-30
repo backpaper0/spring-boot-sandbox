d2628d7e8b386a91616d08f8e979c3271d16e81c
Taichi Uragami <backpaper0@gmail.com>
2017-07-30 13:10:11 +0900

    Immutable JSON object sample

---
+++ spring-sandbox/src/main/java/study/json/immutable/ImmutableJsonSample.java
@@ -0,0 +1,52 @@
+package study.json.immutable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SpringBootApplication
+@RestController
+public class ImmutableJsonSample {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ImmutableJsonSample.class, args);
+    }
+
+    @PostMapping("/emp")
+    Emp get(@RequestBody final Emp emp) {
+        return emp;
+    }
+}
+
+final class Emp {
+
+    public final Integer id;
+    public final String name;
+    public final Dept dept;
+
+    public Emp(
+            @JsonProperty("id") final Integer id,
+            @JsonProperty("name") final String name,
+            @JsonProperty("dept") final Dept dept) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+    }
+}
+
+final class Dept {
+
+    public final Integer id;
+    public final String name;
+
+    public Dept(
+            @JsonProperty("id") final Integer id,
+            @JsonProperty("name") final String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ spring-sandbox/src/test/java/study/json/immutable/ImmutableJsonSampleTest.java
@@ -0,0 +1,44 @@
+package study.json.immutable;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ImmutableJsonSampleTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void test() {
+        final URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + "/emp")
+                .build()
+                .toUri();
+        final String body = "{\"id\":123,\"name\":\"uragami\",\"dept\":{\"id\":456,\"name\":\"development\"}}";
+        final RequestEntity<String> request = RequestEntity.post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(body);
+
+        final ResponseEntity<String> response = template.exchange(request, String.class);
+
+        System.out.println(response.getBody());
+    }
+
+}
