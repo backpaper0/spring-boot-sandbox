43fdc171573e4937617f35367db0b8056ffbd81d
Taichi Uragami <backpaper0@gmail.com>
2017-08-03 22:35:25 +0900

    簡単なテストのサンプル

---
+++ spring-sandbox/src/test/java/study/TestSample.java
@@ -0,0 +1,44 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Objects;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestSample {
+
+    @Test
+    public void test() throws Exception {
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(BarImpl.class);
+        context.register(Foo.class);
+        context.refresh();
+
+        final Foo foo = context.getBean(Foo.class);
+
+        assertThat(foo.bar.get()).isEqualTo("Hello World");
+    }
+
+    static class Foo {
+
+        private final Bar bar;
+
+        public Foo(final Bar bar) {
+            this.bar = Objects.requireNonNull(bar);
+        }
+    }
+
+    interface Bar {
+        String get();
+    }
+
+    static class BarImpl implements Bar {
+
+        @Override
+        public String get() {
+            return "Hello World";
+        }
+    }
+}
