f8c413018042e6398b0495d1a34b71a507daf66c
Taichi Uragami <backpaper0@gmail.com>
2017-08-24 22:01:53 +0900

    昔ながらのファイルアップロード・ダウンロード

---
+++ traditional-file-upload-download-sample/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.xml.bind.ValidationException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "index";
+    }
+
+    @PostMapping("/")
+    @ResponseBody
+    ResponseEntity<StreamingResponseBody> post(@RequestPart final MultipartFile file)
+            throws Exception {
+
+        if (file.isEmpty()) {
+            throw new ValidationException("file was empty");
+        }
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentDispositionFormData("filename", "sample.txt");
+
+        final StreamingResponseBody body = out -> out.write(file.getBytes());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.valueOf("application/force-download"))
+                .body(body);
+    }
+
+    @ExceptionHandler
+    String handle(final Exception e, final Model model) {
+        e.printStackTrace(System.out);
+        model.addAttribute("errorMessage", e.getMessage());
+        return "index";
+    }
+}
