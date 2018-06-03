e27851555874eaa932fbdeaf76f35c1048e06899
Taichi Uragami <backpaper0@gmail.com>
2018-06-03 21:21:53 +0900

    別クラスに切り出した

--- functional-endpoints-example/src/main/java/com/example/FunctionalEndpointsExample.java
+++ functional-endpoints-example/src/main/java/com/example/FunctionalEndpointsExample.java
@@ -1,34 +1,23 @@
 package com.example;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
 
 public class FunctionalEndpointsExample {
 
     public static void main(final String[] args) throws Exception {
 
-        final FunctionalEndpointsExample handlers = new FunctionalEndpointsExample();
-
-        final RouterFunction<ServerResponse> routerFunction = RouterFunctions
-                .route(GET("/hello"), handlers::hello);
+        final RouterFunction<ServerResponse> routerFunction = HelloHandlers.routerFunction();
 
         final HttpHandler handler = RouterFunctions.toHttpHandler(routerFunction);
         final ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
         HttpServer.create("localhost", 8080).newHandler(adapter).block();
 
         Thread.sleep(Long.MAX_VALUE);
-    }
-
-    Mono<ServerResponse> hello(final ServerRequest request) {
-        return ServerResponse.ok().body(Mono.just("Hello, world!"), String.class);
     }
 }
---
+++ functional-endpoints-example/src/main/java/com/example/HelloHandlers.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+public class HelloHandlers {
+
+    Mono<ServerResponse> hello(final ServerRequest request) {
+        return ServerResponse.ok().body(Mono.just("Hello, world!"), String.class);
+    }
+
+    static RouterFunction<ServerResponse> routerFunction() {
+        final HelloHandlers handlers = new HelloHandlers();
+        return RouterFunctions
+                .route(GET("/hello"), handlers::hello);
+    }
+}
