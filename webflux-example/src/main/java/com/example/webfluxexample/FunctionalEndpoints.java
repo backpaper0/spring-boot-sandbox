c6c025d788d40467f41d5d70bfea834881485648
Taichi Uragami <backpaper0@gmail.com>
2018-06-06 05:39:05 +0900

    Add functional endpoints

---
+++ webflux-example/src/main/java/com/example/webfluxexample/FunctionalEndpoints.java
@@ -0,0 +1,37 @@
+package com.example.webfluxexample;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.*;
+
+import java.util.Objects;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class FunctionalEndpoints {
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return route(POST("/add"), this::handle);
+    }
+
+    private Mono<ServerResponse> handle(final ServerRequest request) {
+        // curl localhost:8080/add -d a=2 -d b=3
+
+        final Mono<String> s = request.formData()
+                .flatMapMany(a -> Flux.just("a", "b").map(a::getFirst))
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .map(Objects::toString);
+
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(s, String.class);
+    }
+}
