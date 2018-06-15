32205271343757ada98ffacca14f1f72b7c762e1
Taichi Uragami <backpaper0@gmail.com>
2018-06-16 07:18:24 +0900

    Bump Spring Boot to 2.0.3.RELEASE

--- snakecase/src/main/java/com/example/snakecase/bind/Snake2CamelConfig.java
+++ snakecase/src/main/java/com/example/snakecase/bind/Snake2CamelConfig.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Snake2CamelConfig extends WebMvcConfigurerAdapter {
+public class Snake2CamelConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
