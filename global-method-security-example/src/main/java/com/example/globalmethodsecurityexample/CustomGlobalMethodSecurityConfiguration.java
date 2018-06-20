f2edd7bab34e3bdb4b2a509b59e1317d49226448
Taichi Uragami <backpaper0@gmail.com>
2018-06-21 07:00:48 +0900

    SecuredAnnotationSecurityMetadataSourceの方が便利っぽい

--- global-method-security-example/src/main/java/com/example/globalmethodsecurityexample/CustomGlobalMethodSecurityConfiguration.java
+++ global-method-security-example/src/main/java/com/example/globalmethodsecurityexample/CustomGlobalMethodSecurityConfiguration.java
@@ -1,13 +1,12 @@
 package com.example.globalmethodsecurityexample;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.security.access.ConfigAttribute;
-import org.springframework.security.access.method.AbstractMethodSecurityMetadataSource;
+import org.springframework.security.access.annotation.AnnotationMetadataExtractor;
+import org.springframework.security.access.annotation.SecuredAnnotationSecurityMetadataSource;
 import org.springframework.security.access.method.MethodSecurityMetadataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
@@ -17,30 +16,22 @@ public class CustomGlobalMethodSecurityConfiguration extends GlobalMethodSecurit
 
     @Override
     protected MethodSecurityMetadataSource customMethodSecurityMetadataSource() {
-        return new CustomMethodSecurityMetadataSource();
+        return new SecuredAnnotationSecurityMetadataSource(new CustomAnnotationMetadataExtractor());
     }
 
-    static class CustomMethodSecurityMetadataSource extends AbstractMethodSecurityMetadataSource {
+    static class CustomAnnotationMetadataExtractor
+            implements AnnotationMetadataExtractor<CustomAuthorize> {
 
         @Override
-        public Collection<ConfigAttribute> getAttributes(final Method method,
-                final Class<?> targetClass) {
-            final CustomAuthorize ca = AnnotationUtils
-                    .findAnnotation(method, CustomAuthorize.class);
-            if (ca == null) {
-                return null;
-            }
-            return Arrays.stream(ca.value()).map(this::toConfigAttribute)
+        public Collection<? extends ConfigAttribute> extractAttributes(
+                final CustomAuthorize securityAnnotation) {
+            return Arrays.stream(securityAnnotation.value())
+                    .map(this::toConfigAttribute)
                     .collect(Collectors.toSet());
         }
 
         private ConfigAttribute toConfigAttribute(final Roles r) {
             return () -> "ROLE_" + r.name();
-        }
-
-        @Override
-        public Collection<ConfigAttribute> getAllConfigAttributes() {
-            return null;
         }
     }
 }
