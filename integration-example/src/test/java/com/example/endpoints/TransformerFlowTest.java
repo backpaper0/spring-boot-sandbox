c14db78029f33ec04e1aa727fcbb9c4d8f3f2e84
Taichi Uragami <backpaper0@gmail.com>
2018-07-19 21:53:19 +0900

    もうちょいアノテーションでスマートにテストを書く

--- integration-example/src/test/java/com/example/endpoints/TransformerFlowTest.java
+++ integration-example/src/test/java/com/example/endpoints/TransformerFlowTest.java
@@ -3,29 +3,32 @@ package com.example.endpoints;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TransformerFlow.class)
 public class TransformerFlowTest {
+
+    @Autowired
+    private MessageChannel input;
+    @Autowired
+    private PollableChannel output;
 
     @Test
     public void test() {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
-            context.register(TransformerFlow.class);
-            context.refresh();
+        input.send(MessageBuilder.withPayload("foo").build());
+        input.send(MessageBuilder.withPayload("bar").build());
+        input.send(MessageBuilder.withPayload("baz").build());
 
-            final MessageChannel input = context.getBean("input", MessageChannel.class);
-            input.send(MessageBuilder.withPayload("foo").build());
-            input.send(MessageBuilder.withPayload("bar").build());
-            input.send(MessageBuilder.withPayload("baz").build());
-
-            final PollableChannel output = context.getBean("output", PollableChannel.class);
-            assertEquals("FOO", output.receive().getPayload());
-            assertEquals("BAR", output.receive().getPayload());
-            assertEquals("BAZ", output.receive().getPayload());
-            assertNull(output.receive(0));
-        }
+        assertEquals("FOO", output.receive().getPayload());
+        assertEquals("BAR", output.receive().getPayload());
+        assertEquals("BAZ", output.receive().getPayload());
+        assertNull(output.receive(0));
     }
 }
