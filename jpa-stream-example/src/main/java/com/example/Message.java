3f44e5eee69cb9ad5085fbd002e5218eec933b05
Taichi Uragami <backpaper0@gmail.com>
2018-08-07 07:50:43 +0900

    JPAのStream検索を試す

---
+++ jpa-stream-example/src/main/java/com/example/Message.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "selectAll", query = "SELECT m FROM Message m ORDER BY m.id ASC")
+public class Message implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    public Message() {
+    }
+
+    public Message(final String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null) {
+            return false;
+        } else if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Message other = (Message) obj;
+        return Objects.equals(id, other.id)
+                && Objects.equals(content, other.content);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%s", id, content);
+    }
+}
---
+++ jpa-stream-example/src/main/java/com/example/MessageService.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MessageService {
+
+    private final EntityManager em;
+
+    public MessageService(final EntityManager em) {
+        this.em = Objects.requireNonNull(em);
+    }
+
+    @Transactional
+    public void setUpData() {
+        em.persist(new Message("foo"));
+        em.persist(new Message("bar"));
+        em.persist(new Message("baz"));
+        em.persist(new Message("qux"));
+    }
+
+    @Transactional(readOnly = true)
+    public void printData() {
+        try (Stream<Message> stream = em.createNamedQuery("selectAll", Message.class)
+                .getResultStream()) {
+            stream.forEach(System.out::println);
+        }
+    }
+}
