f7674175a3c9f9f73d01edd5f4a46081f3b489fe
Taichi Uragami <backpaper0@gmail.com>
2018-08-15 23:00:20 +0900

    bean name

---
+++ spring-sandbox/src/main/java/study/beanname/BeanNameExample.java
@@ -0,0 +1,33 @@
+package study.beanname;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class BeanNameExample {
+
+    //何も指定されなければメソッド名がbean nameになる。
+    @Bean
+    public Baz baz1() {
+        return new Baz();
+    }
+
+    //@Beanのname要素が設定されていれば、それがbean nameになる。
+    @Bean(name = "bazbaz")
+    public Baz bar2() {
+        return new Baz();
+    }
+
+    //FQCNがbean nameになる。
+    @Component
+    public static class Foo {
+    }
+
+    @Component("barbar")
+    public static class Bar {
+    }
+
+    public static class Baz {
+    }
+}
---
+++ spring-sandbox/src/test/java/study/beanname/BeanNameExampleTest.java
@@ -0,0 +1,47 @@
+package study.beanname;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import study.beanname.BeanNameExample.Bar;
+import study.beanname.BeanNameExample.Baz;
+import study.beanname.BeanNameExample.Foo;
+
+@SpringJUnitConfig(BeanNameExample.class)
+class BeanNameExampleTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void component() {
+        final String[] actual = context.getBeanNamesForType(Foo.class);
+        final String[] expected = new String[] {
+                "study.beanname.BeanNameExample$Foo"
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void componentValueElement() {
+        final String[] actual = context.getBeanNamesForType(Bar.class);
+        final String[] expected = new String[] {
+                "barbar"
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void beanAnnotation() {
+        final String[] actual = context.getBeanNamesForType(Baz.class);
+        final String[] expected = new String[] {
+                "baz1",
+                "bazbaz"
+        };
+        assertArrayEquals(expected, actual);
+    }
+}
