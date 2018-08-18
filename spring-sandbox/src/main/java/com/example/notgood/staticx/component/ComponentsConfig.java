e31c5646f38d5438d0f812e378a625c3c00c16c8
Taichi Uragami <backpaper0@gmail.com>
2018-08-18 21:15:19 +0900

    まとめちゃお

--- spring-sandbox/src/main/java/com/example/notgood/staticx/component/ComponentsConfig.java
+++ spring-sandbox/src/main/java/com/example/notgood/staticx/component/ComponentsConfig.java
@@ -2,6 +2,10 @@ package com.example.notgood.staticx.component;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import com.example.notgood.staticx.factory.HelloFactory;
 
 @Configuration
 public class ComponentsConfig {
@@ -9,5 +13,10 @@ public class ComponentsConfig {
     @Bean
     public Hello hello() {
         return new Hello("********************%nHello, %s!%n********************%n");
+    }
+
+    @EventListener
+    public void bind(final ContextRefreshedEvent event) {
+        HelloFactory.setHello(hello());
     }
 }
