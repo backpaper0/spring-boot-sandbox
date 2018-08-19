2f29f09757d74747572afdf3fcbbd75cdcb8729d
Taichi Uragami <backpaper0@gmail.com>
2018-08-20 07:26:03 +0900

    ObjectProvider（JSR330のProviderみたいなやつ）

---
+++ spring-sandbox/src/main/java/study/provider/ObjectProviderExample.java
@@ -0,0 +1,9 @@
+package study.provider;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ObjectProviderExample {
+}
