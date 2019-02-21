b2f82274eb234014583a433a0077bd25999ba9bf
Taichi Uragami <backpaper0@gmail.com>
2019-02-21 23:29:15 +0900

    ๛ก(ｰ̀ωｰ́ก)

---
+++ reactor-example/src/test/java/com/example/StepVerifierWithVirtualTimeTest.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+class StepVerifierWithVirtualTimeTest {
+
+    @Test
+    void delay() throws Exception {
+        StepVerifier.withVirtualTime(() -> Mono.delay(Duration.ofSeconds(3)))
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(3))
+                .expectNext(0L)
+                .verifyComplete();
+    }
+
+    @Test
+    void interval() throws Exception {
+        StepVerifier.withVirtualTime(() -> Flux.interval(Duration.ofSeconds(1)).take(3))
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(1))
+                .expectNext(0L)
+                .expectNoEvent(Duration.ofSeconds(1))
+                .expectNext(1L)
+                .expectNoEvent(Duration.ofSeconds(1))
+                .expectNext(2L)
+                .verifyComplete();
+    }
+}
