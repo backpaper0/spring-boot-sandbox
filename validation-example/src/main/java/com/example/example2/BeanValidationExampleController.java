5352ba2b7feb5289880479564de5f68738910aa2
Taichi Uragami <backpaper0@gmail.com>
2019-03-13 05:19:35 +0900

    org.springframework.validation.beanvalidation.SpringValidatorAdapterへの参照

--- validation-example/src/main/java/com/example/example2/BeanValidationExampleController.java
+++ validation-example/src/main/java/com/example/example2/BeanValidationExampleController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * @see org.springframework.validation.beanvalidation.SpringValidatorAdapter
+ */
 @RestController
 @RequestMapping("/2")
 public class BeanValidationExampleController {
