05cb1273c942b63fc980526b0f2ba36b436caba4
Taichi Uragami <backpaper0@gmail.com>
2019-03-13 07:26:08 +0900

    WIP

--- login-validation-example/src/main/java/com/example/LoginValidationFilter.java
+++ login-validation-example/src/main/java/com/example/LoginValidationFilter.java
@@ -1,14 +1,14 @@
 package com.example;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -35,15 +35,14 @@ public class LoginValidationFilter extends OncePerRequestFilter {
             final FilterChain filterChain) throws ServletException, IOException {
 
         if (requestMatcher.matches(request)) {
-            final Set<ConstraintViolation<?>> errors = new LinkedHashSet<>();
-            errors.addAll(validator.validateValue(beanType,
-                    UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY,
-                    request.getParameter(
-                            UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)));
-            errors.addAll(validator.validateValue(beanType,
-                    UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY,
-                    request.getParameter(
-                            UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)));
+
+            final Set<?> errors = Stream
+                    .of(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY,
+                            UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
+                    .flatMap(a -> validator.validateValue(beanType, a, request.getParameter(a))
+                            .stream())
+                    .collect(Collectors.toSet());
+
             if (errors.isEmpty() == false) {
                 //TODO
             }
