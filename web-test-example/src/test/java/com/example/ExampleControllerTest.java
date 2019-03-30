188eaf473e270039082ad25fd304a6f707a5692e
Taichi Uragami <backpaper0@gmail.com>
2019-03-30 20:54:43 +0900

    JUnit 5

--- web-test-example/src/test/java/com/example/ExampleControllerTest.java
+++ web-test-example/src/test/java/com/example/ExampleControllerTest.java
@@ -4,28 +4,28 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
-public class ExampleControllerTest {
+class ExampleControllerTest {
 
     private MockMvc mvc;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         mvc = standaloneSetup(new ExampleController()).build();
     }
 
     @Test
-    public void getHello() throws Exception {
+    void getHello() throws Exception {
         mvc.perform(get("/example"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"));
     }
 
     @Test
-    public void notFound() throws Exception {
+    void notFound() throws Exception {
         mvc.perform(get("/unknown"))
                 .andExpect(status().isNotFound());
     }
