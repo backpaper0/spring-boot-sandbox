3e7283697ce9a888b77f5890ad971a50caca77e5
Uragami Taichi <backpaper0@gmail.com>
2019-06-19 05:29:03 +0900

    JPAのexample

---
+++ jpa-example/src/main/java/com/example/JpaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExampleApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(JpaExampleApplication.class, args);
+    }
+}
