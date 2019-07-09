71b880de5660272cc228cf90cb9bc4f87809209f
Uragami Taichi <backpaper0@gmail.com>
2019-07-09 21:26:27 +0900

    Qualifierのテスト

--- spring-sandbox/src/test/java/study/bean/BeanTest.java
+++ spring-sandbox/src/test/java/study/bean/BeanTest.java
@@ -2,8 +2,13 @@ package study.bean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 class BeanTest {
 
@@ -39,5 +44,94 @@ class BeanTest {
     }
 
     static class Ddd extends Ccc {
+    }
+
+    @Test
+    void testQualifier() throws Exception {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
+            context.register(Ggg.class);
+            context.register(Hhh.class);
+            context.register(Iii.class);
+            context.refresh();
+
+            final Hhh hhh = context.getBean(Hhh.class);
+
+            assertNotNull(hhh.aaa);
+            assertNotNull(hhh.bbb);
+            assertNotNull(hhh.ccc);
+            assertNotNull(hhh.ddd);
+
+            assertTrue(hhh.aaa == hhh.bbb);
+            assertTrue(hhh.aaa == hhh.ccc);
+            assertTrue(hhh.aaa == hhh.ddd);
+
+            final Iii iii = context.getBean(Iii.class);
+
+            assertNotNull(iii.aaa);
+            assertNotNull(iii.bbb);
+            assertNotNull(iii.ccc);
+            assertNotNull(iii.ddd);
+
+            assertTrue(iii.aaa == iii.bbb);
+            assertTrue(iii.aaa == iii.ccc);
+            assertTrue(iii.aaa == iii.ddd);
+
+            assertTrue(hhh.aaa != iii.aaa);
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Qualifier
+    @interface Eee {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Qualifier
+    @interface Fff {
+    }
+
+    static class Ggg {
+
+        @Bean
+        @Eee
+        Ddd ggg() {
+            return new Ddd();
+        }
+
+        @Bean
+        @Fff
+        Ddd fff() {
+            return new Ddd();
+        }
+    }
+
+    static class Hhh {
+
+        Aaa aaa;
+        Bbb bbb;
+        Ccc ccc;
+        Ddd ddd;
+
+        Hhh(@Eee final Aaa aaa, @Eee final Bbb bbb, @Eee final Ccc ccc, @Eee final Ddd ddd) {
+            this.aaa = aaa;
+            this.bbb = bbb;
+            this.ccc = ccc;
+            this.ddd = ddd;
+        }
+    }
+
+    static class Iii {
+
+        Aaa aaa;
+        Bbb bbb;
+        Ccc ccc;
+        Ddd ddd;
+
+        Iii(@Fff final Aaa aaa, @Fff final Bbb bbb, @Fff final Ccc ccc, @Fff final Ddd ddd) {
+            this.aaa = aaa;
+            this.bbb = bbb;
+            this.ccc = ccc;
+            this.ddd = ddd;
+        }
     }
 }
