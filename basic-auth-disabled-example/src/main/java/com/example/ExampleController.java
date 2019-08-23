8186e59db9deb1a4bd492f01afd7c1427f289c84
Uragami Taichi <backpaper0@gmail.com>
2019-08-24 05:41:55 +0900

    Basic認証を無効化する
    というか有効化しない。

---
+++ basic-auth-disabled-example/src/main/java/com/example/ExampleController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ExampleController {
+
+    @GetMapping
+    public String get() {
+        return "Hello World";
+    }
+}
