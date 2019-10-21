4d3488b7ce8d926462ef10dbdfe99674de638dee
Uragami Taichi <backpaper0@gmail.com>
2019-10-21 17:06:12 +0900

    Fix

--- spring-sandbox/src/main/java/study/scope/Counter.java
+++ spring-sandbox/src/main/java/study/scope/Counter.java
@@ -2,15 +2,17 @@ package study.scope;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
+@Component
 @SessionScope
 public class Counter implements Serializable {
 
     private int value;
 
     public int getAndIncrement() {
-        int v = value;
+        final int v = value;
         value++;
         return v;
     }
