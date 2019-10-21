c87a56a8eefc6f33a246742504b8d74bb18f4266
Uragami Taichi <backpaper0@gmail.com>
2019-10-21 17:18:05 +0900

    Move async example to sandbox project

--- spring-sandbox/src/main/java/com/example/async/AsyncExampleApplication.java
+++ spring-sandbox/src/main/java/com/example/async/AsyncExampleApplication.java
@@ -1,9 +1,8 @@
-package com.example;
+package com.example.async;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,22 +10,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class AsyncExampleApplication implements CommandLineRunner {
+public class AsyncExampleApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(AsyncExampleApplication.class, args);
-    }
-
-    private final AsyncMethods methods;
-
-    public AsyncExampleApplication(final AsyncMethods methods) {
-        this.methods = methods;
-    }
-
-    @Override
-    public void run(final String... args) throws Exception {
-        System.out.println(methods.method1().get());
-        System.out.println(methods.method2().get());
     }
 
     @Bean
---
+++ spring-sandbox/src/test/java/com/example/async/AsyncExampleApplicationTest.java
@@ -0,0 +1,24 @@
+package com.example.async;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncExampleApplicationTest {
+
+    @Autowired
+    private AsyncMethods methods;
+
+    @Test
+    void method1() throws Exception {
+        assertEquals("default", methods.method1().get());
+    }
+
+    @Test
+    void method2() throws Exception {
+        assertEquals("alternative executor", methods.method2().get());
+    }
+}
