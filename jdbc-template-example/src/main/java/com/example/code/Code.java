1f45c2ee5dd5826851cc246195157da12ebb69dc
Uragami Taichi <backpaper0@gmail.com>
2019-12-13 07:03:49 +0900

    WIP

---
+++ jdbc-template-example/src/main/java/com/example/code/Code.java
@@ -0,0 +1,8 @@
+package com.example.code;
+
+public interface Code {
+
+    String getCode();
+
+    String getLabel();
+}
---
+++ jdbc-template-example/src/main/java/com/example/code/ExtendedBeanPropertySqlParameterSource.java
@@ -0,0 +1,19 @@
+package com.example.code;
+
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+
+public class ExtendedBeanPropertySqlParameterSource extends BeanPropertySqlParameterSource {
+
+    public ExtendedBeanPropertySqlParameterSource(final Object object) {
+        super(object);
+    }
+
+    @Override
+    public Object getValue(final String paramName) throws IllegalArgumentException {
+        final Object value = super.getValue(paramName);
+        if (value instanceof Code) {
+            return ((Code) value).getCode();
+        }
+        return value;
+    }
+}
