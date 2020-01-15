2ea00668e565703e7eb4cc7653dd78f067437815
Uragami Taichi <backpaper0@gmail.com>
2020-01-15 21:35:26 +0900

    SameSite=Laxの例

---
+++ samesite-cookie-demo/src/main/java/com/example/SameSiteCookieTomcatContextCustomizer.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SameSiteCookieTomcatContextCustomizer implements TomcatContextCustomizer {
+
+    @Override
+    public void customize(final Context context) {
+        final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+        cookieProcessor.setSameSiteCookies("Lax");
+        context.setCookieProcessor(cookieProcessor);
+    }
+}
---
+++ samesite-cookie-demo/src/main/java/com/example/SamesiteCookieDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SamesiteCookieDemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SamesiteCookieDemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String index(final HttpSession session) {
+        return "demo";
+    }
+}
