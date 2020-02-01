e65aa2434cc70d5e2600841482ac3b81f3218c34
Uragami Taichi <backpaper0@gmail.com>
2020-02-01 15:28:38 +0900

    複数のmodelを扱う

--- spring-sandbox/src/main/java/com/example/webmvc/model/ModelExampleController.java
+++ spring-sandbox/src/main/java/com/example/webmvc/model/ModelExampleController.java
@@ -1,5 +1,7 @@
 package com.example.webmvc.model;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,24 @@ public class ModelExampleController {
         return new ModelExampleModel("default");
     }
 
+    @ModelAttribute("foo")
+    ModelExampleModel foo() {
+        return new ModelExampleModel("foo");
+    }
+
+    @ModelAttribute("bar")
+    ModelExampleModel bar() {
+        return new ModelExampleModel("bar");
+    }
+
     @GetMapping("/")
     ModelExampleModel home(final ModelExampleModel model) {
         return model;
+    }
+
+    @GetMapping("/foobar")
+    Object foobar(@ModelAttribute("foo") final ModelExampleModel foo,
+            @ModelAttribute("bar") final ModelExampleModel bar) {
+        return Map.of("foo", foo, "bar", bar);
     }
 }
--- spring-sandbox/src/test/java/com/example/webmvc/model/ModelExampleApplicationTest.java
+++ spring-sandbox/src/test/java/com/example/webmvc/model/ModelExampleApplicationTest.java
@@ -2,11 +2,16 @@ package com.example.webmvc.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.URI;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ModelExampleApplicationTest {
@@ -18,5 +23,15 @@ class ModelExampleApplicationTest {
     void home() throws Exception {
         final ModelExampleModel model = http.getForObject("/", ModelExampleModel.class);
         assertEquals(new ModelExampleModel("default"), model);
+    }
+
+    @Test
+    void foobar() throws Exception {
+        final Object model = http.exchange(RequestEntity.get(URI.create("/foobar")).build(),
+                new ParameterizedTypeReference<Map<String, ModelExampleModel>>() {
+                }).getBody();
+        assertEquals(
+                Map.of("foo", new ModelExampleModel("foo"), "bar", new ModelExampleModel("bar")),
+                model);
     }
 }
