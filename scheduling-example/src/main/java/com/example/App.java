7ddc920aa848cb817a4fb48bde496e9351f35e23
Uragami Taichi <backpaper0@gmail.com>
2020-10-23 06:50:12 +0900

    Update javadoc

--- scheduling-example/src/main/java/com/example/App.java
+++ scheduling-example/src/main/java/com/example/App.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 
+ * @see org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration
+ * @see <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-task-execution-scheduling">22. Task Execution and Scheduling</a>
+ * 
+ */
 @SpringBootApplication
 @EnableScheduling
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
