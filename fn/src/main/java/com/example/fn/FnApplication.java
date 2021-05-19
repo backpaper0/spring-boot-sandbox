ab62679f5c353f6b062d0cc1fa6307cf4caf208b
Uragami Taichi <backpaper0@gmail.com>
2021-05-20 05:31:53 +0900

    Update

--- fn/src/main/java/com/example/fn/FnApplication.java
+++ fn/src/main/java/com/example/fn/FnApplication.java
@@ -1,28 +1,27 @@
 package com.example.fn;
 
 import java.util.function.Function;
-import java.util.function.UnaryOperator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class FnApplication {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(FnApplication.class, args);
-    }
+	public static void main(final String[] args) {
+		SpringApplication.run(FnApplication.class, args);
+	}
 
-    @Bean
-    public Function<String, String> hello() {
-        return s -> String.format("Hello, %s!", s);
-    }
+	@Bean
+	public Function<String, String> hello() {
+		return s -> String.format("Hello, %s!", s);
+	}
 
-    @Bean
-    public UnaryOperator<Flux<String>> upper() {
-        return flux -> flux.map(String::toUpperCase);
-    }
+	@Bean
+	public Function<Mono<String>, Mono<String>> upper() {
+		return mono -> mono.map(String::toUpperCase);
+	}
 }
