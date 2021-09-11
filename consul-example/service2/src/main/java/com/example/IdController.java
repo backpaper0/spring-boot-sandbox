b74cbe70224acf0654c43fd328f7cbd8782f9a29
Uragami Taichi <backpaper0@gmail.com>
2021-09-11 16:46:34 +0900

    Polish

--- consul-example/service2/src/main/java/com/example/IdController.java
+++ consul-example/service2/src/main/java/com/example/IdController.java
@@ -2,20 +2,39 @@ package com.example;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
+import org.springframework.cloud.consul.serviceregistry.ConsulAutoRegistration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/id")
-public class IdController {
+public class IdController implements ApplicationContextAware, InitializingBean {
 
-	@Value("${myapp.id}")
 	private String id;
+	private ApplicationContext context;
+	private final ConsulDiscoveryProperties properties;
+
+	public IdController(ConsulDiscoveryProperties properties) {
+		this.properties = properties;
+	}
 
 	@GetMapping
 	public Object get() {
 		return Map.of("id", id);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		id = ConsulAutoRegistration.getInstanceId(properties, context);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.context = applicationContext;
 	}
 }
