bdd9d67d27cf91ec6d6ab8d6d75e078fce0632cd
Uragami Taichi <backpaper0@gmail.com>
2021-09-12 11:26:02 +0900

    タイムアウト設定

--- consul-example/service1/src/main/java/com/example/App.java
+++ consul-example/service1/src/main/java/com/example/App.java
@@ -1,22 +1,37 @@
 package com.example;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ConfigurationProperties(prefix = "my")
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
+	private Duration connectTimeout;
+	private Duration readTimeout;
+
 	@Bean
 	@LoadBalanced
 	public RestTemplate loadBalancedRestTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+		return builder.setConnectTimeout(connectTimeout).setReadTimeout(readTimeout).build();
+	}
+
+	public void setConnectTimeout(Duration connectTimeout) {
+		this.connectTimeout = connectTimeout;
+	}
+
+	public void setReadTimeout(Duration readTimeout) {
+		this.readTimeout = readTimeout;
 	}
 }
