8e36923bcee0e32cfa4bd4df80c77706dc0b6fce
Uragami Taichi <backpaper0@gmail.com>
2021-09-13 05:42:03 +0900

    コードが古すぎたのでバイバイ

--- es-sample/src/main/java/sample/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SampleRepository extends CrudRepository<Sample, String> {
-    List<Sample> findByText(String text);
-}
