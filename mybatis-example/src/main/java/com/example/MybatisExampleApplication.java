1827f990951b529be837a585a781a117f41ace80
Uragami Taichi <backpaper0@gmail.com>
2022-02-19 08:11:27 +0900

    DatabaseIdProvider

--- mybatis-example/src/main/java/com/example/MybatisExampleApplication.java
+++ mybatis-example/src/main/java/com/example/MybatisExampleApplication.java
@@ -1,12 +1,23 @@
 package com.example;
 
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MybatisExampleApplication {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisExampleApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "app.database-id")
+	public DatabaseIdProvider databaseIdProvider() {
+		return new VendorDatabaseIdProvider();
 	}
 }
