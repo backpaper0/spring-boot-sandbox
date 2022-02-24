cf0c3d3629b5178abe44041cf2380cdc8f211e5c
Uragami Taichi <backpaper0@gmail.com>
2022-02-25 08:46:05 +0900

    タグ

--- metrics-example/src/main/java/com/example/CountController.java
+++ metrics-example/src/main/java/com/example/CountController.java
@@ -16,11 +16,17 @@ public class CountController {
 	private final AtomicInteger counter1 = new AtomicInteger();
 	private final AtomicInteger counter2;
 	private final Counter counter3;
+	private Counter counter4a;
+	private Counter counter4b;
+	private Counter counter4c;
 
 	public CountController(MeterRegistry registry) {
 		Gauge.builder("example.count.1", this.counter1::get).register(registry);
 		this.counter2 = registry.gauge("example.count.2", new AtomicInteger());
 		this.counter3 = registry.counter("example.count.3");
+		this.counter4a = registry.counter("example.count.4", "example.name", "foo");
+		this.counter4b = registry.counter("example.count.4", "example.name", "bar");
+		this.counter4c = registry.counter("example.count.4", "example.name", "baz");
 	}
 
 	@GetMapping("/count")
@@ -28,6 +34,9 @@ public class CountController {
 		counter1.incrementAndGet();
 		counter2.incrementAndGet();
 		counter3.increment();
+		counter4a.increment();
+		counter4b.increment();
+		counter4c.increment();
 		return Map.of("counter1", counter1.get(),
 				"counter2", counter2.get(),
 				"counter3", counter3.count());
