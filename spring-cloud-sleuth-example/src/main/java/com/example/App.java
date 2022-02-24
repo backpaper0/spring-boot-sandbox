7e76d862ea10fc02c8c6bd5dcd48a54bc3e90b73
Uragami Taichi <backpaper0@gmail.com>
2022-02-24 21:12:37 +0900

    sleuth

---
+++ spring-cloud-sleuth-example/src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@EnableAsync
+public class App {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder.build();
+	}
+}
