b0ff80e10f214db880973ef8bdadfaa3518643c3
Uragami Taichi <backpaper0@gmail.com>
2022-02-26 14:27:36 +0900

    さよならWebSecurityConfigurerAdapter

--- oidc-example/src/main/java/com/example/OidcExampleApplication.java
+++ oidc-example/src/main/java/com/example/OidcExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OidcExampleApplication {
 
+	@SuppressWarnings("resource")
 	public static void main(final String[] args) {
 		SpringApplication.run(OidcExampleApplication.class, args);
 	}
--- oidc-example/src/main/java/com/example/WebSecurityConfig.java
+++ oidc-example/src/main/java/com/example/WebSecurityConfig.java
@@ -1,18 +1,21 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.authorizeRequests()
 				.antMatchers("/").permitAll()
 				.anyRequest().authenticated()
 				.and()
-				.oauth2Login();
+				.oauth2Login()
+				.and()
+				.build();
 	}
 }
