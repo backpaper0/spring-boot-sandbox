67708158d321b9eabe7dc8641e34d1c53b9f5c4b
Uragami Taichi <backpaper0@gmail.com>
2022-02-26 14:23:03 +0900

    さよならWebSecurityConfigurerAdapter

--- spring-security-jdbc-login-sample/src/main/java/com/example/DemoApplication.java
+++ spring-security-jdbc-login-sample/src/main/java/com/example/DemoApplication.java
@@ -1,18 +1,7 @@
 package com.example;
 
-import java.util.Objects;
-
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -20,59 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoApplication {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	@SuppressWarnings("resource")
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/hello";
-    }
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/hello";
+	}
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-}
-
-@Configuration
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final DataSource dataSource;
-
-    public WebSecurityConfig(final DataSource dataSource) {
-        this.dataSource = Objects.requireNonNull(dataSource);
-    }
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .usersByUsernameQuery(
-                        "select username, password, true from account where username = ?")
-                .authoritiesByUsernameQuery(
-                        "select username, role from account where username = ?")
-                .groupAuthoritiesByUsername(
-                        "select '', '', '' from account where username = ? and 0 = 1");
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login");
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 }
