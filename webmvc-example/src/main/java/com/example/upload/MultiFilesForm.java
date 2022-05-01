37e2ab31dad43ffe8229747ce413e8e9fb667f8d
Uragami Taichi <backpaper0@gmail.com>
2022-05-01 13:13:34 +0900

    アップロード

---
+++ webmvc-example/src/main/java/com/example/upload/MultiFilesForm.java
@@ -0,0 +1,27 @@
+package com.example.upload;
+
+import java.util.List;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class MultiFilesForm {
+
+	@Size(min = 1)
+	private List<MultipartFile> files = List.of();
+
+	public List<MultipartFile> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<MultipartFile> files) {
+		this.files = files;
+	}
+
+	@AssertTrue
+	public boolean isEmpty() {
+		return files.size() > 0 && files.stream().allMatch(a -> a != null && !a.isEmpty());
+	}
+}
---
+++ webmvc-example/src/main/java/com/example/upload/SingleFileForm.java
@@ -0,0 +1,23 @@
+package com.example.upload;
+
+import javax.validation.constraints.AssertTrue;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class SingleFileForm {
+
+	private MultipartFile file;
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	@AssertTrue
+	public boolean isEmpty() {
+		return file != null && !file.isEmpty();
+	}
+}
