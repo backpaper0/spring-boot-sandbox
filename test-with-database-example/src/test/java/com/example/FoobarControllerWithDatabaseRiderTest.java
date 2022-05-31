1aa3ed686b9f5d6d6ec3548b55b7d1a166f65e9e
Uragami Taichi <backpaper0@gmail.com>
2022-06-01 06:15:27 +0900

    Database RiderとDBUnitを使う

--- test-with-database-example/src/test/java/com/example/FoobarControllerWithDatabaseRiderTest.java
+++ test-with-database-example/src/test/java/com/example/FoobarControllerWithDatabaseRiderTest.java
@@ -7,20 +7,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.junit5.api.DBRider;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class FoobarControllerTest {
+@DBRider
+public class FoobarControllerWithDatabaseRiderTest {
 
 	@Autowired
 	MockMvc mvc;
 
 	@Test
-	@Transactional
-	@Sql("foobar.sql")
+	@DataSet("foobar.yml")
+	@ExpectedDataSet("expectedFoobar.yml")
 	void test() throws Exception {
 		mvc.perform(get("/foobar"))
 				.andExpectAll(status().isOk(),
