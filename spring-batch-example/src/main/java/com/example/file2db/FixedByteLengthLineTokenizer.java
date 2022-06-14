9d20cddb47af5bf3f59b0fb4dbf1dad37bbbca56
Uragami Taichi <backpaper0@gmail.com>
2022-06-15 06:04:04 +0900

    FixedByteLengthLineTokenizerのテストを追加してプロダクションコードも一部を修正

--- spring-batch-example/src/main/java/com/example/file2db/FixedByteLengthLineTokenizer.java
+++ spring-batch-example/src/main/java/com/example/file2db/FixedByteLengthLineTokenizer.java
@@ -1,83 +1,94 @@
 package com.example.file2db;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.batch.item.file.transform.DefaultFieldSet;
 import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.transform.FlatFileFormatException;
 import org.springframework.batch.item.file.transform.LineTokenizer;
 
 public class FixedByteLengthLineTokenizer implements LineTokenizer {
 
-	private final List<Column> columns;
-	private final List<String> names;
+    public static final Charset SHIFT_JIS = Charset.forName("Shift_JIS");
+    public static final Charset MS932 = Charset.forName("MS932");
 
-	public FixedByteLengthLineTokenizer(List<Column> columns) {
-		this.columns = columns;
-		this.names = columns.stream().map(a -> a.name).toList();
-	}
+    private final List<Column> columns;
+    private final List<String> names;
+    private final int totalByteLength;
+    private final Charset charset;
 
-	@Override
-	public FieldSet tokenize(String line) {
-		try {
-			List<String> tokens = new ArrayList<>();
-			byte[] bytes;
-			bytes = line.getBytes("Windows-31J");
-			int index = 0;
-			for (Column column : columns) {
-				String token = new String(bytes, index, column.byteLength, "Windows-31J");
-				tokens.add(column.dataType.apply(token));
-				index += column.byteLength;
-			}
-			return new DefaultFieldSet(tokens.toArray(String[]::new), names.toArray(String[]::new));
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public FixedByteLengthLineTokenizer(List<Column> columns, Charset charset) {
+        this.columns = columns;
+        this.names = columns.stream().map(a -> a.name).toList();
+        this.totalByteLength = columns.stream().mapToInt(a -> a.byteLength).sum();
+        this.charset = charset;
+    }
 
-	public static class Column {
+    @Override
+    public FieldSet tokenize(String line) {
+        List<String> tokens = new ArrayList<>();
+        byte[] bytes;
+        if (line != null) {
+            bytes = line.getBytes(charset);
+        } else {
+            bytes = new byte[0];
+        }
+        if (bytes.length != totalByteLength) {
+            throw new FlatFileFormatException("バイト数が異なります", line);
+        }
+        int index = 0;
+        for (Column column : columns) {
+            String token = new String(bytes, index, column.byteLength, charset);
+            tokens.add(column.dataType.apply(token));
+            index += column.byteLength;
+        }
+        return new DefaultFieldSet(tokens.toArray(String[]::new), names.toArray(String[]::new));
+    }
 
-		private final String name;
-		private final int byteLength;
-		private final DataType dataType;
+    public static class Column {
 
-		public Column(String name, int byteLength, DataType dataType) {
-			this.name = name;
-			this.byteLength = byteLength;
-			this.dataType = dataType;
-		}
-	}
+        private final String name;
+        private final int byteLength;
+        private final DataType dataType;
 
-	public enum DataType {
-		STRING,
-		NUMBER;
+        public Column(String name, int byteLength, DataType dataType) {
+            this.name = name;
+            this.byteLength = byteLength;
+            this.dataType = dataType;
+        }
+    }
 
-		String apply(String value) {
-			switch (this) {
-			case NUMBER: {
-				int index = 0;
-				while (index < value.length()) {
-					if (value.charAt(index) != '0') {
-						break;
-					}
-					index++;
-				}
-				return value.substring(index);
-			}
-			case STRING: {
-				int index = value.length() - 1;
-				while (index > -1) {
-					if (!Character.isSpaceChar(value.charAt(index))) {
-						break;
-					}
-					index--;
-				}
-				return value.substring(0, index + 1);
-			}
-			default:
-				return value;
-			}
-		}
-	}
+    public enum DataType {
+        STRING,
+        NUMBER;
+
+        String apply(String value) {
+            switch (this) {
+            case NUMBER: {
+                int index = 0;
+                while (index < value.length() - 1) {
+                    if (value.charAt(index) != '0') {
+                        break;
+                    }
+                    index++;
+                }
+                return value.substring(index);
+            }
+            case STRING: {
+                int index = value.length() - 1;
+                while (index > -1) {
+                    if (!Character.isSpaceChar(value.charAt(index))) {
+                        break;
+                    }
+                    index--;
+                }
+                return value.substring(0, index + 1);
+            }
+            default:
+                return value;
+            }
+        }
+    }
 }
---
+++ spring-batch-example/src/test/java/com/example/file2db/FixedByteLengthLineTokenizerTest.java
@@ -0,0 +1,80 @@
+package com.example.file2db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.transform.FlatFileFormatException;
+
+import com.example.file2db.FixedByteLengthLineTokenizer.Column;
+import com.example.file2db.FixedByteLengthLineTokenizer.DataType;
+
+/**
+ * FixedByteLengthLineTokenizerのテストコード。
+ *
+ */
+public class FixedByteLengthLineTokenizerTest {
+
+    FixedByteLengthLineTokenizer sut;
+
+    @BeforeEach
+    void init() {
+        List<Column> columns = List.of(
+                new Column("foo", 6, DataType.STRING),
+                new Column("bar", 3, DataType.NUMBER));
+        sut = new FixedByteLengthLineTokenizer(columns, FixedByteLengthLineTokenizer.SHIFT_JIS);
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "hello 001 |  hello |   1",
+            "hello 000 |  hello |   0", // 0
+            "hello 999 |  hello | 999", // 数字の上限
+            "テスト001  | テスト |   1", // 全角文字列
+    })
+    void test(String line, String foo, int bar) {
+        FieldSet fieldSet = sut.tokenize(line);
+        assertEquals(foo, fieldSet.readString("foo"));
+        assertEquals(bar, fieldSet.readInt("bar"));
+    }
+
+    /**
+     * スペースのみで構成される項目は空文字列として扱う。
+     */
+    @Test
+    void testEmptyString() {
+        FieldSet fieldSet = sut.tokenize("      001");
+        assertEquals("", fieldSet.readString("foo"));
+        assertEquals(1, fieldSet.readInt("bar"));
+    }
+
+    /**
+     * nullは空文字列として処理する。
+     */
+    @Test
+    void testNull() {
+        List<Column> columns = List.of();
+        FixedByteLengthLineTokenizer lineTokenizer = new FixedByteLengthLineTokenizer(columns,
+                FixedByteLengthLineTokenizer.SHIFT_JIS);
+        FieldSet fieldSet = lineTokenizer.tokenize(null);
+        assertNotNull(fieldSet);
+    }
+
+    /**
+     * バイト数がカラム定義のバイト数合計よりも多かったり少なかったら例外をスローする。
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "12345612", // 少ない
+            "1234561234", // 多い
+    })
+    void testInvalidByteLength(String line) {
+        assertThrows(FlatFileFormatException.class, () -> sut.tokenize(line));
+    }
+}
