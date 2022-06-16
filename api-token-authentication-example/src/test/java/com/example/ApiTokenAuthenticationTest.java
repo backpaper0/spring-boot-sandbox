1d1c30db3423b189951c42726789090ce565caeb
Uragami Taichi <backpaper0@gmail.com>
2022-06-17 08:25:13 +0900

    テストを追加

---
+++ api-token-authentication-example/src/test/java/com/example/ApiTokenAuthenticationTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.util.LinkedMultiValueMap;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiTokenAuthenticationTest {
+
+	@Autowired
+	TestRestTemplate http;
+
+	@Test
+	void unauthorized() {
+		var responseEntity = http.getForEntity("/demo", Map.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode());
+	}
+
+	@Test
+	void ok() {
+		var form = new LinkedMultiValueMap<>();
+		form.add("username", "test");
+		var response = http.postForObject("/login", form, Map.class);
+		var token = response.get("apiToken").toString();
+		var requestEntity = RequestEntity.get("/demo").header("X-Api-Token", token).build();
+		var responseEntity = http.exchange(requestEntity, Map.class);
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals("test", responseEntity.getBody().get("name"));
+	}
+}
