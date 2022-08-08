50b3cbff2443aad07dff0e71a29be19e7e5e4b32
Uragami Taichi <backpaper0@gmail.com>
2022-08-08 13:12:43 +0900

    一時的なコードが残っていたので削除

--- spring-sandbox/src/test/java/com/example/date/TestApp.java
+++ spring-sandbox/src/test/java/com/example/date/TestApp.java
@@ -1,35 +1,7 @@
 package com.example.date;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApp {
-
-	/** システム日時を取得するインターフェース */
-	@Autowired
-	private LocalDateTimeProvider localDateTimeProvider;
-
-	/** システム日付を取得するインターフェース */
-	@Autowired
-	private LocalDateProvider localDateProvider;
-
-	/** システム時刻を取得するインターフェース */
-	@Autowired
-	private LocalTimeProvider localTimeProvider;
-
-	private void run() {
-		// システム日時の取得
-		LocalDateTime dateTime = localDateTimeProvider.now();
-
-		// システム日付の取得
-		LocalDate date = localDateProvider.now();
-
-		// システム時刻の取得
-		LocalTime time = localTimeProvider.now();
-	}
 }
