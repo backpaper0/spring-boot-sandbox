09c65efdf99a93655285fb200d82769390dfff5c
Uragami Taichi <backpaper0@gmail.com>
2022-08-13 17:30:53 +0900

    Spring Securityが入っておった

--- webmvc-api-error-handling-example/src/test/java/com/example/interceptor/HandlerInterceptorExampleControllerTest.java
+++ webmvc-api-error-handling-example/src/test/java/com/example/interceptor/HandlerInterceptorExampleControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -18,6 +19,7 @@ public class HandlerInterceptorExampleControllerTest {
 	MockMvc mvc;
 
 	@Test
+	@WithMockUser
 	void test() throws Exception {
 
 		assertNull(ClassNameHolder.get());
