adad1ba4e68ad7f4d83b1502e82fc0442e20c3fc
Uragami Taichi <backpaper0@gmail.com>
2022-11-26 07:58:20 +0900

    SpringJUnitConfigアノテーションは要らない

--- jpa-example/src/test/java/com/example/many_to_one/ManyToOneTest.java
+++ jpa-example/src/test/java/com/example/many_to_one/ManyToOneTest.java
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
-@SpringJUnitConfig
 @SpringBootTest
 @Transactional
 class ManyToOneTest {
