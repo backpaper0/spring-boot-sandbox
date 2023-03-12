ac9a9f4b6122c9c71697c46892f1f0d8c4e65fd1
Uragami Taichi <backpaper0@gmail.com>
2023-03-12 17:52:20 +0900

    PathRequest.toH2Console()でH2コンソールのRequestMatcherを取得できるっぽい

--- security-and-h2console-example/src/main/java/com/example/security/WebSecurityConfig.java
+++ security-and-h2console-example/src/main/java/com/example/security/WebSecurityConfig.java
@@ -1,25 +1,17 @@
 package com.example.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class WebSecurityConfig {
-
-	@Autowired
-	private HandlerMappingIntrospector mvcHandlerMappingIntrospector;
-	@Autowired
-	private ObjectPostProcessor<Object> opp;
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,7 +19,7 @@ public class WebSecurityConfig {
 		// any request && !h2 console
 		AndRequestMatcher requestMatcher = new AndRequestMatcher(
 				AnyRequestMatcher.INSTANCE,
-				new NegatedRequestMatcher(h2console()));
+				new NegatedRequestMatcher(PathRequest.toH2Console()));
 
 		return http
 				.securityMatcher(requestMatcher)
@@ -39,18 +31,12 @@ public class WebSecurityConfig {
 	@Bean
 	SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.securityMatcher(h2console())
+				.securityMatcher(PathRequest.toH2Console())
 				.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
 				.csrf(csrf -> csrf.disable())
 				.headers(headers -> {
 					headers.frameOptions().sameOrigin();
 				})
 				.build();
-	}
-
-	private MvcRequestMatcher h2console() {
-		return opp.postProcess(new MvcRequestMatcher.Builder(mvcHandlerMappingIntrospector)
-				.servletPath("/h2-console")
-				.pattern("/**"));
 	}
 }
