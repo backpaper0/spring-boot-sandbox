05beb6f438ceb776caa2cdff3f5e263c8c778f01
Uragami Taichi <backpaper0@gmail.com>
2024-01-11 07:45:05 +0900

    デフォルトの規則に従うならMappedCollectionアノテーションは要らないっぽい

--- spring-data-jdbc-example/src/main/java/com/example/entity/Singer.java
+++ spring-data-jdbc-example/src/main/java/com/example/entity/Singer.java
@@ -4,13 +4,12 @@ import java.util.Set;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 
 public record Singer(
 		@Id Integer id,
 		String name,
 		@Version Integer version,
-		@MappedCollection Set<Song> songs) {
+		Set<Song> songs) {
 
 	public Singer withName(String newName) {
 		return new Singer(id, newName, version, songs);
