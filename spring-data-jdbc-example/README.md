77902ed369163fea568fcfaf7a739c195cfcc889
Uragami Taichi <backpaper0@gmail.com>
2024-01-10 20:46:38 +0900

    1度のクエリーで集約を構築するやつを試す

--- spring-data-jdbc-example/README.md
+++ spring-data-jdbc-example/README.md
@@ -1,3 +1,19 @@
+# Spring Data JDBCの例
 
-- `org.springframework.data.jdbc.core.convert.DefaultDataAccessStrategy`
+## 構成
 
+Mavenのプロファイル機能でデフォルトならH2、`pg`プロファイルならPostgreSQLに関する依存関係が設定されるようにしている。
+
+PostgreSQLの場合、Spring BootのDocker Composeサポート機能を使用して自動でPostgreSQLのコンテナが起動するようにしている。
+また、テスト実行時にもコンテナを起動させたいためMaven Surefire Pluginの`argLine`プロパティで`spring.docker.compose.skip.in-tests`を`false`に設定している。
+
+## 動作確認
+
+ウィンドウ関数を利用して集約を1度のクエリーで取得する。
+PostgreSQLを使用（H2はウィンドウ関数が実装されていないため）。
+
+```bash
+mvn -Ppg test -Dtest=#singer1
+```
+
+ログを見ると1度のクエリーで取得していることがわかる。
--- spring-data-jdbc-example/src/main/java/com/example/App.java
+++ spring-data-jdbc-example/src/main/java/com/example/App.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
+import org.springframework.lang.Nullable;
 
 @SpringBootApplication
-public class App {
+public class App implements BeanPostProcessor {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	@Nullable
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof JdbcMappingContext c) {
+			// ウィンドウ関数を利用して集約を1度のクエリーで取得する。
+			// https://docs.spring.io/spring-data/relational/reference/jdbc/entity-persistence.html#jdbc.loading-aggregates
+			c.setSingleQueryLoadingEnabled(true);
+		}
+		return bean;
 	}
 }
