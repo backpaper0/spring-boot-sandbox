1fd81b5b7149058c457346f33ff418efe2efff49
Uragami Taichi <backpaper0@gmail.com>
2024-02-16 20:54:07 +0900

    非推奨メソッド使わない

--- security-two-authn-api-example/src/main/java/com/example/config/SecurityConfig.java
+++ security-two-authn-api-example/src/main/java/com/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -15,42 +16,42 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
-		InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager(
-				User.withUsername("alice").password(passwordEncoder().encode("secret")).authorities("USER").build());
-		DaoAuthenticationProvider ap = new DaoAuthenticationProvider();
-		ap.setPasswordEncoder(passwordEncoder());
-		ap.setUserDetailsService(uds);
-		return http
-				.securityMatcher("/customers/**")
-				.authorizeHttpRequests(c -> c.anyRequest().authenticated())
-				.authenticationProvider(ap)
-				.csrf(c -> c.disable())
-				.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.httpBasic(c -> c.and())
-				.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+        InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager(
+                User.withUsername("alice").password(passwordEncoder().encode("secret")).authorities("USER").build());
+        DaoAuthenticationProvider ap = new DaoAuthenticationProvider();
+        ap.setPasswordEncoder(passwordEncoder());
+        ap.setUserDetailsService(uds);
+        return http
+                .securityMatcher("/customers/**")
+                .authorizeHttpRequests(c -> c.anyRequest().authenticated())
+                .authenticationProvider(ap)
+                .csrf(c -> c.disable())
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-		UserDetailsService uds = new InMemoryUserDetailsManager(
-				User.withUsername("bob").password(passwordEncoder().encode("secret")).authorities("USER").build());
-		DaoAuthenticationProvider ap = new DaoAuthenticationProvider();
-		ap.setPasswordEncoder(passwordEncoder());
-		ap.setUserDetailsService(uds);
-		return http
-				.securityMatcher("/orders/**")
-				.authorizeHttpRequests(c -> c.anyRequest().authenticated())
-				.authenticationProvider(ap)
-				.csrf(c -> c.disable())
-				.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.httpBasic(c -> c.and())
-				.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        UserDetailsService uds = new InMemoryUserDetailsManager(
+                User.withUsername("bob").password(passwordEncoder().encode("secret")).authorities("USER").build());
+        DaoAuthenticationProvider ap = new DaoAuthenticationProvider();
+        ap.setPasswordEncoder(passwordEncoder());
+        ap.setUserDetailsService(uds);
+        return http
+                .securityMatcher("/orders/**")
+                .authorizeHttpRequests(c -> c.anyRequest().authenticated())
+                .authenticationProvider(ap)
+                .csrf(c -> c.disable())
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
