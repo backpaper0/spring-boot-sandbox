0604ac5c899f74fddcf1d34d6144452bf4283dc6
Uragami Taichi <backpaper0@gmail.com>
2024-02-17 05:22:54 +0900

    非推奨なメソッドを使わない

--- functional-endpoints-example/src/test/java/com/example/PersonHandlersTest.java
+++ functional-endpoints-example/src/test/java/com/example/PersonHandlersTest.java
@@ -8,22 +8,22 @@ import com.example.PersonHandlers.Person;
 
 class PersonHandlersTest {
 
-	WebTestClient client = WebTestClient
-			.bindToRouterFunction(PersonHandlers.routerFunction())
-			.build();
+    WebTestClient client = WebTestClient
+            .bindToRouterFunction(PersonHandlers.routerFunction())
+            .build();
 
-	@Test
-	void getPerson() {
-		client.get().uri("/people/hoge").exchange()
-				.expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
-				.expectBody().jsonPath("name", "hoge");
-	}
+    @Test
+    void getPerson() {
+        client.get().uri("/people/hoge").exchange()
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("name", "hoge");
+    }
 
-	@Test
-	void postPerson() {
-		client.post().uri("/people").contentType(MediaType.APPLICATION_JSON)
-				.syncBody(new Person("foobar")).exchange()
-				.expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
-				.expectBody().jsonPath("name", "foobar");
-	}
+    @Test
+    void postPerson() {
+        client.post().uri("/people").contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new Person("foobar")).exchange()
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("name", "foobar");
+    }
 }
