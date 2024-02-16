b09cf0f077790a1904e37c51181662942126905e
Uragami Taichi <backpaper0@gmail.com>
2024-02-16 20:35:00 +0900

    AsyncResultはdeprecatedらしい

--- spring-sandbox/src/main/java/com/example/async/AsyncMethods.java
+++ spring-sandbox/src/main/java/com/example/async/AsyncMethods.java
@@ -1,21 +1,21 @@
 package com.example.async;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AsyncMethods {
 
-	@Async
-	public Future<String> method1() {
-		return AsyncResult.forValue(Thread.currentThread().getName());
-	}
+    @Async
+    public Future<String> method1() {
+        return CompletableFuture.completedFuture(Thread.currentThread().getName());
+    }
 
-	@Async("altExecutor")
-	public Future<String> method2() {
-		return AsyncResult.forValue(Thread.currentThread().getName());
-	}
+    @Async("altExecutor")
+    public Future<String> method2() {
+        return CompletableFuture.completedFuture(Thread.currentThread().getName());
+    }
 }
