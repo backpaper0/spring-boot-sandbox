68e8b833ec219a65706e473f2fc6e4c5154f2e6d
Uragami Taichi <backpaper0@gmail.com>
2024-03-23 21:18:23 +0900

    データベース操作のテレメトリー

--- telemetry-example/src/main/java/com/example/Dice.java
+++ telemetry-example/src/main/java/com/example/Dice.java
@@ -1,9 +1,10 @@
 package com.example;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
@@ -12,12 +13,21 @@ import io.opentelemetry.instrumentation.annotations.WithSpan;
 @Component
 public class Dice {
 
+    private final JdbcClient jdbc;
+
+    public Dice(JdbcClient jdbc) {
+        this.jdbc = jdbc;
+    }
+
     @WithSpan
     public List<Integer> rollTheDice(@SpanAttribute int rolls) {
         return IntStream.range(0, rolls).map(a -> rollOnce()).boxed().toList();
     }
 
     private int rollOnce() {
-        return ThreadLocalRandom.current().nextInt(1, 7);
+        // return ThreadLocalRandom.current().nextInt(1, 7);
+        return jdbc.sql("select floor(rand() * 6) + 1")
+                .query(SingleColumnRowMapper.newInstance(Integer.class))
+                .single();
     }
 }
